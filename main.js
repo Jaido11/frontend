0ecfbdb93e51947490e63d0b92df5236463f5b64
Jaido11 <djayobut@gmail.com>
2023-05-20 21:44:41 +0800

    Fixing line endings

--- main.js
+++ main.js
@@ -1,25 +1,29 @@
-const { app, BrowserWindow } = require("electron");
+// Imported Modules
+const { app, BrowserWindow, ipcMain} = require("electron");
 const path = require("path");
-
+const axios = require("axios");
+// Main Window
 const isDev = true;
 
 const createWindow = () => {
   const win = new BrowserWindow({
-    width: isDev ? 1200 : 600,
+    width: isDev ? 1000 : 500,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
   });
 
-  if (isDev) {
-    win.webContents.openDevTools();
-  }
+  win.webContents.openDevTools();
 
   win.loadFile(path.join(__dirname, "./renderer/index.html"));
 };
 
 app.whenReady().then(() => {
+  // Initialize Function
+  ipcMain.handle('axios.openAI',openAI)
+
+
   createWindow();
 
   app.on("activate", () => {
@@ -34,3 +38,36 @@ app.on("window-all-closed", () => {
     app.quit();
   }
 });
+
+
+
+async function openAI(event, sentence){
+  let res = null;
+
+  await axios({
+    method: 'post',
+    url: 'https://api.openai.com/v1/completions',
+    data: {
+    model: "text-davinci-003",
+    prompt: "Correct this to standard English:\n\n" + sentence,
+    temperature: 0,
+    max_tokens: 60,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0
+    },
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer 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'
+    
+    }
+  }).then(function (response) {
+    res = response.data;
+  })
+  .catch(function (error) {
+    res = error;
+  });
+
+
+  return res;
+}
